fc3c8a252c5e70557ed4450feb931ef19e0cb9be
J3LopezL <jl.lopez77@uniandes.edu.co>
2022-04-03 16:01:47 -0500

    Aplicaction for aws BeansTalk

--- application.py
+++ application.py
@@ -6,22 +6,25 @@ from flaskr.models.models import db
 from flask_jwt_extended import JWTManager
 from flask_cors import CORS
 
-app = create_app('default')
-app_context = app.app_context()
+application = create_app('default')
+app_context = application.app_context()
 app_context.push()
 
-db.init_app(app)
+db.init_app(application)
 db.create_all()
 
-cors = CORS(app)
-jwt = JWTManager(app)
-api = Api(app)
+cors = CORS(application)
+jwt = JWTManager(application)
+api = Api(application)
 
 # Punto de arranque: gunicorn
 def gunicorn():
     # Retornar el objeto de la aplicacion
-    return app
+    return application
 
 api.add_resource(ViewHealthCheck, '/api/healthcheck')
 api.add_resource(ViewBlacklists, '/api/blacklists')
 api.add_resource(ViewBlacklist, '/api/blacklists/<string:blacklist_email>')
+
+if __name__ == "__main__":
+    application.run(port = 5000, debug = True)
--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -5,14 +5,14 @@ import os
 def create_app(config_name):
     app = Flask(__name__)
 
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_HOST')
+    app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:SXG0n2BHHnkOOWrxseMh@db-devops.cew5sozmzl0x.us-east-1.rds.amazonaws.com:5432/entrega1"
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['JWT_SECRET_KEY']='frase-secreta'
     app.config['JWT_ACCESS_TOKEN_EXPIRES']=timedelta(days=1)
     app.config['PROPAGATE_EXCEPTIONS'] = True
 
     if(app.config['SQLALCHEMY_DATABASE_URI'] == "" or app.config['SQLALCHEMY_DATABASE_URI'] is None):
-        app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql+psycopg2://postgres:changeme@localhost:5433/BlackListBD"
+        app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///entrega1.db"
         
         
     return app
